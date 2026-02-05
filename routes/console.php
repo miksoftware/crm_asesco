153b3c6a16b37f19f8edb732ad462b9d581c6241
Marileidy afanador <mafanadord@uts.edu.co>
2026-02-04 20:12:18 -0500

    new cron notifications

--- routes/console.php
+++ routes/console.php
@@ -13,3 +13,15 @@ Schedule::command('chats:sync --limit=50')
     ->everyFiveMinutes()
     ->withoutOverlapping()
     ->runInBackground();
+
+// Sincronizar notificaciones cada 10 minutos (limpia huérfanas y sincroniza con mensajes)
+Schedule::command('notifications:sync')
+    ->everyTenMinutes()
+    ->withoutOverlapping()
+    ->runInBackground();
+
+// Arreglar mensajes no leídos que ya fueron respondidos (cada hora)
+Schedule::command('messages:fix-unread')
+    ->hourly()
+    ->withoutOverlapping()
+    ->runInBackground();
